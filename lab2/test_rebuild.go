3bda1767bcf67aa94cfb87ed8a0ed8f45045410d
marcheanin <marcheanin1576@gmail.com>
2024-02-26 14:52:24 +0300

    add lab2

---
+++ lab2/test_rebuild.go
@@ -0,0 +1,32 @@
+package test
+
+import "fmt"
+
+var q string
+var w string
+var z bool
+var y bool
+
+func sum(a, b int) {
+	var e int
+	var t int
+	fmt.Println(e + t + a + b)
+}
+
+func main() {
+	fmt.Println("da")
+	var gt int
+	var a int
+	var d int
+	var b int
+	var nm string
+
+	for i := 0; i < 10; i++ {
+		var l int
+		var m int
+		fmt.Println(l, m)
+	}
+
+	sum(a, b)
+	fmt.Println(d, gt, nm, q, w, z, y)
+}
